f45cea98b86333bff938f8682277696b744658cd
Daniel Gil Alegre <dgil@mincer.es>
2021-01-14 15:44:08 +0100

    Cambio de URL de repositorio

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
         host: 'localhost',
-        user: 'antonio',
-        password: 'admin',
+        user: 'root',
+        password: 'root',
         database: 'contacts'
     }
 };
